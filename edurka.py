8f5f6c9f3602e1d9644ef001ae80176cc03b7745
PeyyalaSirisha <peyyalasirisha@gmail.com>
2024-11-07 11:45:12 +0530

    Add files via upload

---
+++ edurka.py
@@ -0,0 +1,30 @@
+Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('helo world')
+helo world
+>>> a=10
+>>> for i in range(0,a)
+SyntaxError: expected ':'
+>>> a=10
+>>> for i in range(a):
+...     b=("*"*i)
+... 
+...     
+>>> print(b)
+*********
+>>> for i in range(0,a):
+...     b=("*"*i)
+... 
+...     
+>>> print(b)
+*********
+>>> mkdir
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    mkdir
+NameError: name 'mkdir' is not defined
+>>> print(edureka.py)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    print(edureka.py)
+NameError: name 'edureka' is not defined
